41227b61e77f1e6c9b1df207c0ae8b51c7684d36
MPPerdigao <margarida.p.perdigao@gmail.com>
2023-02-06 18:23:02 +0000

    lab 2 update and structure corrected

--- Lab_1/app.py
+++ Lab_1/app.py
@@ -1,8 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
-#comment
+
 @app.route('/')
 def hello():
     return "Hello World!"
 if __name__ == '__main__':
     app.run()
+
+from flask import render_template, request
+
+@app.route ('/form', methods = ['GET', 'POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        return 'Hello'+ name
+    return render_template ('form.html')
